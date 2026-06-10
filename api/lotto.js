a509e7e2f91c42334c4d8e6405f08518e8e50669
jhleeeeeeeeeee <ljhkaalee1018@gmail.com>
2026-06-10 14:22:36 +0900

    Update lotto.js

--- api/lotto.js
+++ api/lotto.js
@@ -1,3 +1,4 @@
+export const config = { runtime: 'edge', regions: ['icn1'] };
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
